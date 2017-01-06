788f2c47ee803460146cd09cc0cd4771151d91a1
Eran <Eran@DESKTOP-J8IDTJ9>
2017-01-06 14:04:57 +0200

    asdfsdf

--- Ass3private/application/Main.java
+++ Ass3private/application/Main.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-//SDJASDASJDASIODJIOASJ
+//SDJASDASJDASIODJIOASJasdfasdfasd
 	@FXML
 	private TextField idfield;
 	private static Stage primaryStage;
