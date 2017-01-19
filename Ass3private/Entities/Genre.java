4dc75cb6f7e9852c40e89729f8ec5997ea47451a
Sagi <Sagi@Sagi-PC>
2017-01-19 20:16:51 +0200

    pornhub.com

---
+++ Ass3private/Entities/Genre.java
@@ -0,0 +1,62 @@
+package Entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Genre extends GeneralMessage implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	public static ArrayList<Genre> genreList;
+	private String genre, comments, oldGenre;
+	public String query;
+	private int bookNum;
+	public ArrayList<Theme> themeList;
+	
+	public Genre(){	
+		genreList=new ArrayList<Genre>();
+		this.setComments("");
+		this.setBookNum(0);
+		}
+	
+	public Genre(String genre){
+		this.genre=genre;
+	}
+	
+	public Genre(String genre,int bookNum,String comments) {
+		this.genre = genre;
+		this.bookNum=bookNum;
+		this.comments=comments;
+	}	
+	
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public int getBookNum() {
+		return bookNum;
+	}
+
+	public void setBookNum(int bookNum) {
+		this.bookNum = bookNum;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+
+	public String getOldGenre() {
+		return oldGenre;
+	}
+
+	public void setOldGenre(String oldGenre) {
+		this.oldGenre = oldGenre;
+	}
+
+}
